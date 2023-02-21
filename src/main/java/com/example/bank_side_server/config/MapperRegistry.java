5b19aa17086e166e7e2f8a9257c09ff0b5932970
daumsimac <daumsimac@gmail.com>
2023-02-21 20:50:01 +0900

    ConnectionPool is added to Retrofit

--- src/main/java/com/example/bank_side_server/config/MapperRegistry.java
+++ src/main/java/com/example/bank_side_server/config/MapperRegistry.java
@@ -3,6 +3,7 @@ package com.example.bank_side_server.config;
 import com.example.bank_side_server.net.PostsAPI;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import okhttp3.ConnectionPool;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import org.springframework.beans.factory.annotation.Value;
@@ -33,8 +34,9 @@ public class MapperRegistry {
 
     @Bean
     PostsAPI getJsonPlaceHolderAPI(){
+        ConnectionPool connectionPool = new ConnectionPool(5, 5L, TimeUnit.MINUTES);
         //retrofit 상세 설정
-        OkHttpClient client = new OkHttpClient.Builder()
+        OkHttpClient client = new OkHttpClient.Builder().connectionPool(connectionPool)
                 //서버로 요청하는데 걸리는 시간을 제한 (20초 이내에 서버에 요청이 성공해야 한다. (handshakre))
                 .connectTimeout(20, TimeUnit.SECONDS)
                 //서버로 요청을 성공했고, 응답데이터를 받는데 시간을 제한한다.(15초 이내로 응답 데이터를 받아야한다.)
